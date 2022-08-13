e355d2fb64cbe8d00084f3c78429e6e62bb2fd17
Dhiraj Gede <dhiraj.gede@rsllearning.com>
2022-08-13 23:38:06 +0530

    fixed autocomplete by filling filteredArray by using EventListner.

--- JS/index.js
+++ JS/index.js
@@ -1,50 +1,44 @@
 const toggler = document.querySelector('.dropdown-menu');
-const ProductArray = ['Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Core Cutter Apparatus','Sieve Shaker Hand Operated', 'Pocket Penetrometer','Automatic Soil Compaction'];
-let filteredArray = [];
-const searchBarContent= document.querySelector('#searchBar');
-// for(let i=0; i<ProductArray.length; i++){
-//     let inputText='';
-//     searchBarContent.addEventListener('keyup', function(e){
-//         inputText=e.target.value.toLowerCase();
-//         console.log(e.target.value);
-//         if(e.target.value===ProductArray[i].toLowerCase()){
-//             filteredArray.push(ProductArray[i]);
-//         }
-//     console.log(filteredArray);
+const ProductArray = ['Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Permeability Tester', 'Core Cutter Apparatus', 'Sieve Shaker Hand Operated', 'Pocket Penetrometer', 'Automatic Soil Compaction'];
+let filteredArray = new Set();
+const searchBarContent = document.querySelector('#searchBar');
 
-        
-//     });
-// }
-
-
-searchBarContent.onkeyup=(e)=>{
-    filterBySearch(e.target.value);
-};
 let counter = 0;
-const itemHTML = (item)=>`<li class="dropdown-item_${counter}">${item}</li>`;
+const itemHTML = (item) => `<li class="dropdown-item_${counter}">${item}</li>`;
 
-const filterBySearch = (query= '')=>{
-    if(query.length===0){
-        toggler.style.display="none";
-    }
-    else{
-        toggler.style.display="block";
-        
-    }
-    let renderHTML = ``;
-    ProductArray.forEach((product)=>{
-        if(product.toLowerCase().includes(query.toLowerCase())){
-            filteredArray.push(product);
-            renderHTML += itemHTML(product);
-            counter++;
-        }
-        
-    })
-    toggler.innerHTML = renderHTML;
-}
-
-filterBySearch();
 let pressThis = document.querySelector('.ThisButton');
-pressThis.addEventListener('click', (e)=>{
+searchBarContent.addEventListener('keyup', (e = '') => {
+
+    filteredArray.clear();
+   
+    let query = e.target.value;
+    console.log(query);
+    if (query.length === 0) {
+        filteredArray.clear();
+    }
+    
+    else {
+        toggler.style.display="block";
+
+        ProductArray.map((product) => {
+            if (product.toLowerCase().includes(query.toLowerCase()) ) {
+                filteredArray.add(product);
+            }
+        }).join('');
+    }
+renderer();
+
     console.log(filteredArray);
 })
+function renderer(){
+    let renderHTML = ``;
+    filteredArray.forEach((product) => {
+        renderHTML += itemHTML(product);
+
+        counter++;
+    console.log(product);
+
+    })
+    toggler.innerHTML = renderHTML;
+
+} 
