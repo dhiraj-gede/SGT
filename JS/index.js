559a027c8aa72abb826e5d01b01f1378a84101fe
Dhiraj Gede <dhiraj.gede@rsllearning.com>
2022-08-13 06:39:27 +0530

    Added autocomplete to searchbar (TODO fix autocomplete by filling filteredArray by using EventListner.

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,50 @@
 const toggler = document.querySelector('.dropdown-menu');
-const ProductArray = ['Permeability Tester','Core Cutter Apparatus','Sieve Shaker Hand Operated', 'Pocket Penetrometer','Automatic Soil Compaction'];
-toggler.innerHTML = `${ProductArray.map(product => `<li><a class="dropdown-item" href="#">${product}</a></li>`).join('')}`;;
+const ProductArray = ['Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Permeability Tester','Core Cutter Apparatus','Sieve Shaker Hand Operated', 'Pocket Penetrometer','Automatic Soil Compaction'];
+let filteredArray = [];
+const searchBarContent= document.querySelector('#searchBar');
+// for(let i=0; i<ProductArray.length; i++){
+//     let inputText='';
+//     searchBarContent.addEventListener('keyup', function(e){
+//         inputText=e.target.value.toLowerCase();
+//         console.log(e.target.value);
+//         if(e.target.value===ProductArray[i].toLowerCase()){
+//             filteredArray.push(ProductArray[i]);
+//         }
+//     console.log(filteredArray);
 
-// TODO: Add autocomplete to the search bar
+        
+//     });
+// }
+
+
+searchBarContent.onkeyup=(e)=>{
+    filterBySearch(e.target.value);
+};
+let counter = 0;
+const itemHTML = (item)=>`<li class="dropdown-item_${counter}">${item}</li>`;
+
+const filterBySearch = (query= '')=>{
+    if(query.length===0){
+        toggler.style.display="none";
+    }
+    else{
+        toggler.style.display="block";
+        
+    }
+    let renderHTML = ``;
+    ProductArray.forEach((product)=>{
+        if(product.toLowerCase().includes(query.toLowerCase())){
+            filteredArray.push(product);
+            renderHTML += itemHTML(product);
+            counter++;
+        }
+        
+    })
+    toggler.innerHTML = renderHTML;
+}
+
+filterBySearch();
+let pressThis = document.querySelector('.ThisButton');
+pressThis.addEventListener('click', (e)=>{
+    console.log(filteredArray);
+})
